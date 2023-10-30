fafd31da3009b145c02c37116a74abe0e7d11f21
Sangnn <sangnnph14292@fpt.edu.vn>
2023-10-30 22:49:27 +0700

    update

--- src/components/FiveDayWeather/index.jsx
+++ src/components/FiveDayWeather/index.jsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.css";
 
 export const FiveDayWeather = () => {
   const [forecast, setForecast] = useState([]);
-  const [isLoading, setIsLoading] = useState(true); 
+  const [isLoading, setIsLoading] = useState(true);
 
   const apiKey = "5d724a92f04ed3bea360323546c88261";
   const city = "Hanoi";
@@ -13,19 +13,25 @@ export const FiveDayWeather = () => {
     )
       .then((response) => response.json())
       .then((data) => {
-        setForecast(data.list);
-        setIsLoading(false); 
+        const currentDate = new Date();
+        const futureDates = data.list.filter((item) => {
+          // Lọc các mốc thời gian trong tương lai, bỏ qua ngày hiện tại
+          const itemDate = new Date(item.dt_txt);
+          return itemDate > currentDate;
+        });
+        setForecast(futureDates);
+        setIsLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching 5-day forecast:", error);
-        setIsLoading(false); 
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <div className={styles.container}>
-      <h2>5-Day Forecast</h2> 
-      {isLoading ? ( 
+      <h2>5-Day Forecast</h2>
+      {isLoading ? (
         <div>Loading...</div>
       ) : (
         <div>
@@ -50,11 +56,11 @@ export const FiveDayWeather = () => {
                         />
                         <div className={styles.tempmax}>
                           {item.main.temp_max}°C
-                          <p>{(item.main.temp_min)}°C</p>
+                          <p>{item.main.temp_min}°C</p>
                         </div>
                         <div className={styles.description}>
-                          {<p>{item.weather[0].description}</p>}
-                          {<p>{item.pop} %</p>}
+                          <p>{item.weather[0].description}</p>
+                          <p>{item.pop} %</p>
                         </div>
                       </div>
                     </li>
